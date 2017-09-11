490ccb00ba3c5d1a8c6526c6c73e2e3a34d020da
Greg Lovvorn <greg.lovvorn@gmail.com>
2017-09-10 20:26:55 -0600

    ch ch ch changes!

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-mkdir "c:\src\handmade\build"
-pushd "c:\src\handmade\build"
-cl -FC -Zi "c:\src\handmade\code\win32_handmade.cpp" user32.lib gdi32.lib
+mkdir "c:\src\from scratch\build"
+pushd "c:\src\from scratch\build"
+cl -Zi "c:\src\from scratch\code\win32_handmade.cpp" user32.lib gdi32.lib
 popd
--- win32_handmade.cpp
+++ win32_handmade.cpp
@@ -5,14 +5,6 @@
 #define local_persist static
 #define global_variable static
 
-//TODO(greg): this is global for now
-global_variable bool Running;
-global_variable BITMAPINFO BitmapInfo;
-global_variable void *BitmapMemory;
-global_variable int BitmapWidth;
-global_variable int BitmapHeight;
-global_variable int BytesPerPixel = 4;
-
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
@@ -23,16 +15,29 @@ typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
 
-internal void RenderWeirdGradient(int XOffset, int YOffset)
-{
-    int Width = BitmapWidth;
+//TODO(greg): this is global for now
+global_variable bool Running;
 
-    int Pitch = Width*BytesPerPixel;
-    uint8 *Row = (uint8 *)BitmapMemory;
-    for(int Y = 0; Y < BitmapHeight; Y++)
+struct win32_offscreen_buffer
+{
+    BITMAPINFO Info;
+    void *Memory;
+    int Width;
+    int Height;
+    int Pitch;
+    int BytesPerPixel;
+};
+
+internal void RenderWeirdGradient(win32_offscreen_buffer Buffer, int XOffset, int YOffset)
+{
+
+    int Width = Buffer.Width;
+
+    uint8 *Row = (uint8 *)Buffer.Memory;
+    for (int Y = 0; Y < Buffer.Height; Y++)
     {
         uint32 *Pixel = (uint32 *)Row;
-        for(int X = 0; X < BitmapWidth; X++)
+        for (int X = 0; X < Width; X++)
         {
             uint8 Blue = (X + XOffset);
             uint8 Green = (Y + YOffset);
@@ -42,100 +47,106 @@ internal void RenderWeirdGradient(int XOffset, int YOffset)
             *Pixel++;
         }
 
-        Row += Pitch;
+        Row += Buffer.Pitch;
     }
 }
 
-internal void Win32ResizeDIBSection(int Width, int Height)
+internal void Win32ResizeDIBSection(win32_offscreen_buffer *Buffer, int Width, int Height)
 {
     // TODO(greg): bulletproof this
     // Maybe don't free first, free after, then free first if that fails
 
-    if(BitmapMemory)
+    if (Buffer->Memory)
     {
-        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+        VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
     }
 
-    BitmapWidth = Width;
-    BitmapHeight = Height;
+    Buffer->Width = Width;
+    Buffer->Height = Height;
 
-    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
-    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
-    BitmapInfo.bmiHeader.biPlanes = 1;
-    BitmapInfo.bmiHeader.biBitCount = 32;
-    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
+    Buffer->Info.bmiHeader.biWidth = Buffer->Width;
+    Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
+    Buffer->Info.bmiHeader.biPlanes = 1;
+    Buffer->Info.bmiHeader.biBitCount = 32;
+    Buffer->Info.bmiHeader.biCompression = BI_RGB;
 
-    int BytesPerPixel = 4;
-    int BitmapMemorySize = (Width*Height)*BytesPerPixel;
-    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+    int BitmapMemorySize = (Width * Height) * Buffer->BytesPerPixel;
+    Buffer->Memory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+
+    Buffer->Pitch = Width * Buffer->BytesPerPixel;
 
     // TODO(greg): Clear this to black
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, RECT ClientRect, int X, int Y, int Width, int Height)
+internal void Win32DisplayBufferInWindow(HDC DeviceContext, RECT ClientRect, win32_offscreen_buffer Buffer, int X, int Y, int Width, int Height)
 {
     int WindowWidth = ClientRect.right - ClientRect.left;
     int WindowHeight = ClientRect.bottom - ClientRect.top;
 
     StretchDIBits(DeviceContext,
-        /*
+                  /*
         X, Y, Width, Height,
         X, Y, Width, Height,
         */
-        0, 0, BitmapWidth, BitmapHeight,
-        0, 0, WindowWidth, WindowHeight,
-        BitmapMemory,
-        &BitmapInfo,
-        DIB_RGB_COLORS, SRCCOPY);
+                  0, 0, Buffer.Width, Buffer.Height,
+                  0, 0, WindowWidth, WindowHeight,
+                  Buffer.Memory,
+                  &Buffer.Info,
+                  DIB_RGB_COLORS, SRCCOPY);
 }
 
 LRESULT CALLBACK Win32MainWindowCallback(HWND Window,
-                                    UINT Message,
-                                    WPARAM WParam,
-                                    LPARAM LParam)
+                                         UINT Message,
+                                         WPARAM WParam,
+                                         LPARAM LParam)
 {
     LRESULT Result = 0;
-  
+
     switch (Message)
     {
-        case WM_SIZE:
-        {
-            RECT ClientRect;
-            GetClientRect(Window, &ClientRect);
-            int Height = ClientRect.bottom - ClientRect.top;
-            int Width = ClientRect.right - ClientRect.left;
-            Win32ResizeDIBSection(Width, Height);
-        } break;
-        case WM_DESTROY:
-        {
-            //TODO(greg): recreate window?
-            Running = false;
-        } break;
-        case WM_CLOSE:
-        {
-            //TODO(greg): Handle this with a confirmation message
-            Running = false;
-        } break;
-        case WM_ACTIVATEAPP:
-        {
-
-        } break;
-        case WM_PAINT:
-        {
-            PAINTSTRUCT Paint;
-            HDC DeviceContext = BeginPaint(Window, &Paint);
-            int x = Paint.rcPaint.left;
-            int y = Paint.rcPaint.top;
-            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            Win32UpdateWindow(DeviceContext, Paint.rcPaint, x, y, Width, Height);
-            EndPaint(Window, &Paint);
-        } break;
-        default:
-        {
-            Result = DefWindowProc(Window, Message, WParam, LParam);
-        } break;
+    case WM_SIZE:
+    {
+        RECT ClientRect;
+        GetClientRect(Window, &ClientRect);
+        int Height = ClientRect.bottom - ClientRect.top;
+        int Width = ClientRect.right - ClientRect.left;
+        Win32ResizeDIBSection(Width, Height);
+    }
+    break;
+    case WM_DESTROY:
+    {
+        //TODO(greg): recreate window?
+        Running = false;
+    }
+    break;
+    case WM_CLOSE:
+    {
+        //TODO(greg): Handle this with a confirmation message
+        Running = false;
+    }
+    break;
+    case WM_ACTIVATEAPP:
+    {
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT Paint;
+        HDC DeviceContext = BeginPaint(Window, &Paint);
+        int x = Paint.rcPaint.left;
+        int y = Paint.rcPaint.top;
+        int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+        int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+        Win32UpdateWindow(DeviceContext, Paint.rcPaint, x, y, Width, Height);
+        EndPaint(Window, &Paint);
+    }
+    break;
+    default:
+    {
+        Result = DefWindowProc(Window, Message, WParam, LParam);
+    }
+    break;
     }
 
     return Result;
@@ -157,12 +168,12 @@ int CALLBACK WinMain(
 
     if (RegisterClass(&WindowClass))
     {
-        HWND Window = 
+        HWND Window =
             CreateWindowEx(
                 0,
                 WindowClass.lpszClassName,
                 "Handmade Hero",
-                WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                 CW_USEDEFAULT,
                 CW_USEDEFAULT,
                 CW_USEDEFAULT,
@@ -178,10 +189,10 @@ int CALLBACK WinMain(
             int YOffset = 0;
 
             Running = true;
-            while(Running)
+            while (Running)
             {
                 MSG Message;
-                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
+                while (PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                 {
                     if (Message.message == WM_QUIT)
                     {
